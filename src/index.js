41dddc45ae8afeb94b16085e6d1f0204aba01afa
github username <github email address>
2019-05-16 16:17:48 +0100

    it doesn't still create a new one from the form

--- src/index.js
+++ src/index.js
@@ -19,6 +19,10 @@ addBtn.addEventListener('click', () => {
 
 // OR HERE!
 
+//find form
+const formEl = document.querySelector(".add-toy-form")
+
+//add individual toy and update likes to DOM
 function addToy(toy) {
 
   const div = document.createElement('div')
@@ -34,17 +38,34 @@ function addToy(toy) {
 
    const likeElement = div.querySelector(".likes")
    const likeBtn = div.querySelector('.like-btn')
- likeBtn.addEventListener('click', function (event) {
-   const newLikes = toy.likes ++
 
+   likeBtn.addEventListener('click', function (event) {
+   const newLikes = toy.likes ++
    likeElement.innerText = `${newLikes} Likes`
  })
 }
 
+//add all toys to DOM
 function addToys (toys) {
-  toys.forEach(toy => { addToy(toy) })
+  toys.forEach(toy => { addToy(toy)})
 }
 
+//update server with like count
+function updateLikes (toy) {
+  const url = `http://localhost:3000/toys/${toy.id}`
+
+  const options =  {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(toy.likes)
+  }
+  return fetch(url, options).then(resp => resp.json())
+}
+
+//get all toys to json
 function getToys() {
 	return fetch('http://localhost:3000/toys')
 		.then(resp => resp.json())
@@ -52,3 +73,28 @@ function getToys() {
 
 getToys()
   .then(toys => addToys(toys))
+
+//create individual toy
+const toy = {
+  name: formEl.name.value,
+  image: formEl.image.value,
+  likes: 0
+  
+  createToy(toy)
+	    .then(toy => addBook(toy))
+}
+
+function createToy (toy) {
+
+  const url = 'http://localhost:3000/toys'
+
+  const options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toy)
+  }
+
+  return fetch(url, options).then(resp => resp.json())
+}
